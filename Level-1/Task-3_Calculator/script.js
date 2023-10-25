e14ff9969b84fde1f9581683b6377f37bda8ebec
murad-18 <tikkamasala286@gmail.com>
2023-10-25 15:10:23 +0500

    completed the task, if needed then changes will be done afterwards

--- Level-1/Task-3_Calculator/script.js
+++ Level-1/Task-3_Calculator/script.js
@@ -2,28 +2,33 @@ function clearall() {
   document.getElementById("result").value = "";
   document.getElementById("equation").value = "";
   document.getElementById("result").style.opacity = "0";
-  document.getElementById("equation").style.fontSize = "6rem";
+  document.getElementById("equation").style.fontSize = "5rem";
   document.getElementById("equation").style.top = "50px";
   document.getElementById("equation").style.left = "20px";
 }
 
-// This function displays the values
 function display(value) {
   document.getElementById("equation").value += value;
 }
 
-// This function evaluates the expression and returns the result
 function calculate() {
   var eqVal = document.getElementById("equation").value;
   var evaluate = eval(eqVal);
   document.getElementById("result").value = `=${evaluate}`;
   resultDisplay();
 }
+
+function remove() {
+  let equation = document.getElementById("equation");
+  let newValue = equation.value.slice(0, -1);
+  equation.value = newValue;
+}
+
 function resultDisplay() {
   document.getElementById("result").style.opacity = "1";
   document.getElementById("result").style.transition =
     "opacity 0.5s ease-in-out";
-  document.getElementById("result").style.fontSize = "6rem";
+  document.getElementById("result").style.fontSize = "5rem";
   document.getElementById("equation").style.fontSize = "3rem";
   document.getElementById("equation").style.transition =
     "font-size 0.5s ease-in-out";
